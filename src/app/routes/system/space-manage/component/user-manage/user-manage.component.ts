907a5451abc4d3cb0ab54665757682af70bd0b18
cyzhu <TNoTa993>
2018-09-29 16:12:55 +0800

    阶段提交

--- src/app/routes/system/space-manage/component/user-manage/user-manage.component.ts
+++ src/app/routes/system/space-manage/component/user-manage/user-manage.component.ts
@@ -46,7 +46,7 @@ import { AddUserComponent } from './components/add-user.component';
   ],
 })
 export class UserManageComponent implements OnInit {
-  arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16];
 
   constructor(private nzModal: NzModalService) {}
   ngOnInit() {}
