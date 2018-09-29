24949335bb158536083b6ce686e7470d6c6a52dd
cyzhu <TNoTa993>
2018-09-29 16:44:03 +0800

    角色管理

--- src/app/routes/system/space-manage/component/role-manage/role-manage.component.ts
+++ src/app/routes/system/space-manage/component/role-manage/role-manage.component.ts
@@ -16,6 +16,26 @@ import { AddRoleComponent } from './components/add-role.component';
         float: right;
         padding-right: 16px;
       }
+      .card-content {
+        width: 300px;
+        background-color: #fff;
+        box-sizing: border-box;
+        border: solid 1px #e8e8e8;
+      }
+      .card-content:hover {
+        box-shadow: 0 0 8px 3px #e8e8e8;
+        width: 300px;
+        background-color: #fff;
+        box-sizing: border-box;
+      }
+      .card-handle {
+        padding: 8px;
+        background-color: #ececec;
+      }
+      .card-handle div {
+        text-align: center;
+        font-size: larger;
+      }
     `
   ]
 })
