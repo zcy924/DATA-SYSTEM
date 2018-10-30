d1d3191679644ad299a5cf23247da75a57f1c3c8
sincisco <1158889685@qq.com>
2018-10-30 08:46:37 +0800

    修改build过程中出错的部分

--- src/designer/layout/designer.header.component.ts
+++ src/designer/layout/designer.header.component.ts
@@ -8,6 +8,7 @@ import { CommService } from '../service/comm.service';
 import { designerStorage } from '../utils/designer.storage';
 import { Router } from '@angular/router';
 import { Destroyable } from '../interface/destroyable';
+import { FilterTools, HelperTools, MoreTools } from './overlay.template';
 
 @Component({
   selector: 'app-designer-header',
@@ -70,7 +71,7 @@ export class DesignerHeaderComponent extends Destroyable implements AfterViewIni
     });
   }
 
-  doDownload(){
+  doDownload() {
     const blob = new Blob([JSON.stringify(session.currentPage.save(), null, 2)], { type: 'text/plain;charset=utf-8' });
 
     FileSaver.saveAs(blob, `zijin.template.${moment().format('YYYYMMDDHHmmss')}.json`);
@@ -96,22 +97,23 @@ export class DesignerHeaderComponent extends Destroyable implements AfterViewIni
     reader.readAsText(file.files[0]);
   }
 
-  change(event: Event) {
+  addImage(event: Event) {
     const option: any = {
-      preserveAspectRatio: false,
-      backgroundColor: undefined,
-      borderStyle: 'solid',
-      borderColor: '#aaa',
-      borderWidth: 0,
-      borderRadius: 0,
-      image: {
-        width: 400,
-        height: 300,
-        url: '',
-        dataUrl: '',
+        preserveAspectRatio: false,
+        backgroundColor: undefined,
+        borderStyle: 'solid',
+        borderColor: '#ccc',
+        borderWidth: 0,
+        borderRadius: 0,
+        image: {
+          width: 400,
+          height: 300,
+          url: '',
+          dataUrl: '',
+        },
       },
-    };
-    const file: HTMLInputElement = <HTMLInputElement>event.currentTarget;
+      file = <HTMLInputElement>event.currentTarget;
+
     if (!file.files || !file.files[0]) {
       return;
     }
@@ -125,7 +127,7 @@ export class DesignerHeaderComponent extends Destroyable implements AfterViewIni
         option.image.width = (<HTMLImageElement>this).naturalWidth;
         option.image.height = (<HTMLImageElement>this).naturalHeight;
         if (session.currentPage) {
-          const ret = graphicFactory.newGraphicByName(session.currentPage, 'imageAuxiliary', 200, 200, option);
+          graphicFactory.newGraphicByName(session.currentPage, 'imageAuxiliary', 200, 200, option);
         }
       };
 
@@ -172,150 +174,6 @@ export class DesignerHeaderComponent extends Destroyable implements AfterViewIni
   }
 
 }
-
-
-const HelperTools = `
-<div class="m-overlay  bottom"
-     style="display:none;z-index: 405; position: absolute; top: 68px; left: 455px;">
-  <div class="m-classMenu " style="zoom: 1;">
-    <div class="placeholder" style="height: 68px;"></div>
-    <ul class="btns-box">
-      <li class="btn-item draggable">
-        <div class="u-tool-btn">
-          <i class="u-icn u-icn-menubar-rect"></i>
-          <span class="text">矩形</span>
-        </div>
-      </li>
-      <li class="btn-item draggable" draggable="true" data-component-name="textAuxiliary">
-        <div class="u-tool-btn">
-          <i class="u-icn u-icn-menubar-text"></i>
-          <span class="text">文本</span>
-        </div>
-      </li>
-      <li class="btn-item draggable">
-        <div class="u-tool-btn">
-          <i class="u-icn u-icn-tab"></i>
-          <span class="text">Tab</span>
-        </div>
-      </li>
-      <li class="btn-item draggable" draggable="true" data-component-name="commentAuxiliary">
-        <div class="u-tool-btn">
-          <i class="u-icn u-icn-menubar-comment"></i>
-          <span class="text">注释框</span>
-        </div>
-      </li>
-    </ul>
-  </div>
-</div>
-`;
-
-const FilterTools = `
-<div class="m-overlay  bottom"
-     style="display:none; z-index: 406; position: absolute; top: 68px; left: 564px;">
-  <div class="m-classMenu " style="zoom: 1;">
-    <div class="placeholder" style="height: 68px;left: 12px;right: 12px;"></div>
-    <ul class="btns-box">
-      <li class="btn-item dragable">
-        <div class="u-tool-btn">
-          <i class="u-icn u-icn-list"></i>
-          <span class="text">列表筛选</span>
-        </div>
-      </li>
-      <li class="btn-item dragable">
-        <div class="u-tool-btn">
-          <i class="u-icn u-icn-range"></i>
-          <span class="text">范围筛选</span>
-        </div>
-      </li>
-      <li class="btn-item dragable">
-        <div class="u-tool-btn">
-          <i class="u-icn u-icn-menubar-date"></i>
-          <span class="text">日期筛选</span>
-        </div>
-      </li>
-      <li class="btn-item dragable">
-        <div class="u-tool-btn">
-          <i class="u-icn u-icn-treeFilter"></i>
-          <span class="text">树状筛选</span>
-        </div>
-      </li>
-    </ul>
-  </div>
-
-</div>
-`;
-
-const MoreTools = `
-<div class="m-overlay  bottom" style="display:none; z-index: 406; position: absolute; top: 68px; left: 342px;">
-  <!--inlcude-->
-  <div class="m-classMenu m-classMenu-more" style="zoom: 1;">
-    <div class="placeholder" style="height: 68px;left: 94px;right: 94px;"></div>
-    <ul class="btns-box">
-    <li class="btn-item draggable" draggable="true" data-component-name="lineChart">
-        <div class="u-tool-btn">
-          <i class="u-icn u-icn-chart-line"></i>
-          <span class="text">折线图</span>
-        </div>
-      </li>
-      <li class="btn-item draggable" draggable="true" data-component-name="clock">
-        <div class="u-tool-btn">
-          <i class="u-icn u-icn-chart-area"></i>
-          <span class="text">区域图</span>
-        </div>
-      </li>
-      <li class="btn-item draggable" draggable="true" data-component-name="pieChart">
-        <div class="u-tool-btn">
-          <i class="u-icn u-icn-chart-pie"></i>
-          <span class="text">饼状图</span>
-        </div>
-      </li>
-      <li class="btn-item dragable">
-        <div class="u-tool-btn">
-          <i class="u-icn u-icn-menubar-meter"></i>
-          <span class="text">仪表图</span>
-        </div>
-      </li>
-      <li class="btn-item dragable">
-        <div class="u-tool-btn">
-          <i class="u-icn u-icn-menubar-indicator"></i>
-          <span class="text">指标卡</span>
-        </div>
-      </li>
-      <li class="btn-item dragable">
-        <div class="u-tool-btn">
-          <i class="u-icn u-icn-menubar-funnel"></i>
-          <span class="text">漏斗图</span>
-        </div>
-      </li>
-      <li class="btn-item dragable">
-        <div class="u-tool-btn">
-          <i class="u-icn u-icn-menubar-radar"></i>
-          <span class="text">雷达图</span>
-        </div>
-      </li>
-      <li class="btn-item dragable">
-        <div class="u-tool-btn">
-          <i class="u-icn u-icn-menubar-sankey"></i>
-          <span class="text">桑基图</span>
-        </div>
-      </li>
-      <li class="btn-item dragable">
-        <div class="u-tool-btn">
-          <i class="u-icn u-icn-menubar-heatmap"></i>
-          <span class="text">热力地图</span>
-        </div>
-      </li>
-      <li class="btn-item dragable">
-        <div class="u-tool-btn">
-          <i class="u-icn u-icn-menubar-sunburst"></i>
-          <span class="text">旭日图</span>
-        </div>
-      </li>
-    </ul>
-  </div>
-</div>
-
-`;
 
 class PopupWrapper {
   private readonly _$element: JQuery;
@@ -441,3 +299,5 @@ class GrabHelper {
 }
 
 export const grabHelper = new GrabHelper(grabTemplate);
+
+
