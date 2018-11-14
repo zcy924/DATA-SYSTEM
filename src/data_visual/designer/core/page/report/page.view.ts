f9a732fe36461968c2b08b637fa0b5cb906069e0
sincisco <1158889685@qq.com>
2018-11-14 16:38:14 +0800

    调整目录结构

--- src/data_visual/designer/core/page/report/page.view.ts
+++ src/data_visual/designer/core/page/report/page.view.ts
@@ -1,28 +1,143 @@
 import { contextMenuHelper } from '../../../../utils/contextMenu';
 import { ReportPageInner } from './page.inner';
-import { AbstractPageView } from './abstract.page.view';
 import { repaintMaskGenerator } from '../../../helper/mask.helper';
 import { boxSelectHelper } from '../../../helper/box.select.helper';
+import { PageConfig } from '../../../../shared/core/page/page.config';
+import { View } from '../../structure/view';
 
+const TEMPLATE = `
+    <div class="report-region">
+        <div class="report-canvas">
+          <div class="report-box">
+             <div class="report-grid">
+             <div class="u-edit-mask">
+                <div class="mask mask-left" tabindex="-1"></div>
+                <div class="mask mask-right" tabindex="-1"></div>
+                <div class="mask mask-bottom" tabindex="-1"></div>
+                <div class="mask mask-top" tabindex="-1"></div>
+              </div>
+             </div>
+          </div>
+        </div>
+    </div>
+`;
 
-export class PageView extends AbstractPageView {
+export class PageView extends View {
 
+  $element: JQuery;
+  protected readonly _$canvas: JQuery;
+  protected readonly _$box: JQuery;
+  $grid: JQuery;
 
   private _contextMenuGenerator: Function;
+
+  repaintMask: Function;
+
+  private _scale = 1;
+  private _width: number;
+  private _height: number;
 
   constructor(private _page: ReportPageInner) {
     super();
 
+    const $element = this.$element = $(TEMPLATE);
+
+    this._$canvas = $element.find('.report-canvas');
+    this._$box = $element.find('.report-box');
+    this.$grid = $element.find('.report-grid');
+
     this.repaintMask = repaintMaskGenerator(this.$element.find('.u-edit-mask'));
+
+    this._init();
   }
 
-  protected _init(){
+  protected _init() {
     this.$grid.attr('draggable', 'true');
     this._bind();
   }
 
+  /**
+   * 获取画布相对于文档的偏移值
+   * @returns {JQuery.Coordinates | undefined}
+   */
+  offset() {
+    return this.$grid.offset();
+  }
+
+  get scale() {
+    return this._scale;
+  }
+
+  set scale(param: number) {
+    this._scale = param / 100;
+    this._refresh();
+  }
+
+  public accept(model: PageConfig) {
+    model.register('remove.backgroundClass', (key, oldValue, newValue) => {
+      this._$box.removeClass('background1 background2 background3 background4');
+    });
+    model.register('add.backgroundClass backgroundClass', (key, oldValue, newValue) => {
+      this._$box.removeClass(oldValue).addClass(newValue);
+    });
+    model.register('remove.backgroundCustom', (key, oldValue, newValue) => {
+      this._$box.css({
+        backgroundImage: `none`,
+      });
+    });
+    model.register('add.backgroundCustom backgroundCustom', (key, oldValue, newValue) => {
+      console.log(newValue);
+      newValue.dataUrl && this._$box.css({
+        backgroundImage: `url(${newValue.dataUrl})`,
+      });
+    });
+    model.register('remove.backgroundColor', (key, oldValue, newValue) => {
+      this._$box.css({
+        backgroundColor: 'transparent',
+      });
+    });
+    model.register('add.backgroundColor backgroundColor', (key, oldValue, newValue) => {
+      this._$box.css({
+        backgroundColor: newValue,
+      });
+    });
+    model.register('auxiliaryLine', (key, oldValue, newValue) => {
+      this.$grid.toggleClass('help-lines', newValue);
+    });
+    model.register('width add.width', (key, oldValue, newValue) => {
+      this._width = newValue;
+      this._refresh();
+    });
+    model.register('height add.height', (key, oldValue, newValue) => {
+      this._height = newValue;
+      this._refresh();
+    });
+  }
+
+  enterFullScreen() {
+    this._$box[0].requestFullscreen();
+  }
+
   set contextMenuGenerator(generator: Function) {
     this._contextMenuGenerator = generator;
+  }
+
+  private _refresh() {
+    if (this._width && this._height) {
+      this.$element.css({
+        'width': this._width * this.scale + 50,
+        'height': this._height * this.scale + 30,
+      });
+      this._$canvas.css({
+        width: this._width * this.scale,
+        height: this._height * this.scale,
+      });
+      this._$box.css('transform', `translate(-50%, -50%) scale(${this.scale})`);
+      this.$grid.css({
+        width: this._width,
+        height: this._height,
+      });
+    }
   }
 
   // 试图到模型
