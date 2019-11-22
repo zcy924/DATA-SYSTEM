38b463af955b6e7157c4f02f7097af6457eb1386
sincisco <1158889685@qq.com>
2019-11-22 17:22:54 +0800

    调整目录结构

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,12 @@
 const { src, dest } = require('gulp');
-
+const ts = require('gulp-typescript');
 const array = [
   'src/data_visual/component.packages/custom',
   'src/data_visual/component.packages/standard',
   'src/data_visual/data.source.packages/mock',
   'src/data_visual/runtime',
   'src/data_visual/shared',
-], aa = [
+], filePattern = [
   '/**/*.less',
   '/**/*.d.ts',
   '/**/*.svg',
@@ -14,7 +14,7 @@ const array = [
   '/**/*.js',
   '/**/*.js.map',
   '/package.json',
-  '/README.md'
+  '/README.md',
 ];
 
 
@@ -23,9 +23,9 @@ function copy(srcArray, b) {
     .pipe(dest('output/' + b + '/'));
 }
 
-exports.copy = async() => {
-  array.forEach((value, index, array) => {
-    const qq = value.split('/');
-    copy(aa.map(value1 => value + value1), qq[qq.length - 1]);
-  });
+exports.default = async () => {
+  // array.forEach((value, index, array) => {
+  //   const qq = value.split('/');
+  //   copy(aa.map(value1 => value + value1), qq[qq.length - 1]);
+  // });
 };
